50902398764124ab0683780df86f0c53f8f9f3fe
Arushichauhan07 <chauhanarushi0709@gmail.com>
2025-01-24 15:25:03 +0530

    changes

--- backend2/index.js
+++ backend2/index.js
@@ -9,9 +9,12 @@ const bodyParser = require("body-parser");
 const app = express();
 require("dotenv").config()
 
-app.use(cors([{
-    origin: ['https://requin-sample-project-jjd9.vercel.app', 'https://requin-sample-project.vercel.app','http://localhost:3000']
-}]));
+app.use(
+    cors({
+      origin: "https://requin-sample-project-jjd9.vercel.app", // Frontend URL
+      credentials: true, // Enable cookies
+    })
+  );
 
 app.use(bodyParser.json())
 app.use(express.json())
